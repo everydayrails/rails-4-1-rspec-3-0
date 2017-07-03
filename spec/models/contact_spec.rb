ecd8fc4e9c94f47c7d4743c35839b011b0390886
Robert Harężlak <harezlak.robert7@gmail.com>
2017-07-03 14:05:27 +0200

    record be_valid & little hack

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,7 +1,12 @@
 require 'rails_helper'
 
 describe Contact do
-  it 'is valid with firstname, lastname and email'
+  it 'is valid with firstname, lastname and email' do
+    contact = Contact.create(firstname: 'Adam', lastname: "Januszowski", email: 'yey@g.com')
+
+    expect(contact).to be_valid  
+  end
+  
   it 'is invalid without firstname'
   it 'is invalid without lastname'
   it 'is invalid without email'
