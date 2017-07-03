440adaa8bbd33c544ea15a76ee080fdf83a36952
Robert Harężlak <harezlak.robert7@gmail.com>
2017-07-03 13:11:13 +0200

    configure for rails 5

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -13,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 20140916022631) do
 
-  create_table "contacts", force: true do |t|
+  create_table "contacts", force: :cascade do |t|
     t.string   "firstname"
     t.string   "lastname"
     t.string   "email"
@@ -21,7 +20,7 @@ ActiveRecord::Schema.define(version: 20140916022631) do
     t.datetime "updated_at"
   end
 
-  create_table "news_releases", force: true do |t|
+  create_table "news_releases", force: :cascade do |t|
     t.string   "title"
     t.date     "released_on"
     t.text     "body"
@@ -29,17 +28,16 @@ ActiveRecord::Schema.define(version: 20140916022631) do
     t.datetime "updated_at"
   end
 
-  create_table "phones", force: true do |t|
+  create_table "phones", force: :cascade do |t|
     t.integer  "contact_id"
     t.string   "phone"
     t.string   "phone_type"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.index ["contact_id"], name: "index_phones_on_contact_id"
   end
 
-  add_index "phones", ["contact_id"], name: "index_phones_on_contact_id"
-
-  create_table "users", force: true do |t|
+  create_table "users", force: :cascade do |t|
     t.string   "email"
     t.string   "password_digest"
     t.datetime "created_at"
