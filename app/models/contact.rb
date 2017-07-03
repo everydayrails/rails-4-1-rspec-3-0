7d565ddb7f0abf7e47a8bc6d6ef31062584a3842
Robert Harężlak <harezlak.robert7@gmail.com>
2017-07-03 14:40:38 +0200

    contact model specs & whitespaces

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -8,7 +8,7 @@ class Contact < ActiveRecord::Base
   # validates :phones, length: { is: 3 }
 
   def name
-    [firstname, lastname].join(' ')
+    "#{firstname} #{lastname}"
   end
 
   def self.by_letter(letter)
--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -7,9 +7,44 @@ describe Contact do
     expect(contact).to be_valid  
   end
   
-  it 'is invalid without firstname'
-  it 'is invalid without lastname'
-  it 'is invalid without email'
-  it 'is invalid with a duplicate email address'
-  it 'returns a contact full name as a string'
+  it 'is invalid without firstname' do
+    #setup
+    contact = Contact.create(firstname: nil)
+    #association
+    contact.valid?
+    #exec
+    expect(contact.errors[:firstname]).to include("can't be blank")
+  end
+  
+  it 'is invalid without lastname' do
+    contact = Contact.create(firstname: nil)
+    
+    contact.valid?
+    
+    expect(contact.errors[:lastname]).to include("can't be blank")
+  end
+
+  it 'is invalid without email' do
+    contact = Contact.create(email: nil)
+
+    contact.valid?
+
+    expect(contact.errors[:email]).to include("can't be blank")
+  end  
+
+  it 'is invalid with a duplicate email address' do
+    Contact.create(firstname: 'Joe', lastname: 'Pesco', email: 'janusz@gmail.com')
+    contact = Contact.create(firstname: 'Hxc', lastname: 'Ps', email: 'janusz@gmail.com')
+
+    contact.valid?
+
+    expect(contact.errors[:email]).to include('has already been taken')
+  end
+
+  it 'returns a contact full name as a string' do
+    contact = Contact.create(firstname: "Bogdan", lastname: "Powstaniec", email: "kpo@sa.com")
+    
+    expect(contact.name).to eq 'Bogdan Powstaniec'
+  end
+  
 end
